e187756924aefa52140e6731ff55918ab8ebbbc5
ngvgi <anthonyjude554@gmail.com>
2020-10-21 17:27:39 +0300

    Updated Login and SignUp route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { SignupComponent } from '@app/auth/signup/signup.component'
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent
-  }
-];
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: 'signup', pathMatch: 'full' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
